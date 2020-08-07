11e5b904053540093ceaa8b050954bd0dac4c215
Николай Миронов <sirtemerin@gmail.com>
2020-08-07 09:33:49 +0000

    Add ifndef

--- bytePlus.h
+++ bytePlus.h
@@ -1,3 +1,6 @@
+#ifndef BYTEPLUS_H
+#define BYTEPLUS_H
+
 #include <QByteArray>
 
 #define INVERT_BYTE(a)   ((a&1)<<7) | ((a&2)<<5) | ((a&4)<<3) | ((a&8)<<1) | ((a&16)>>1) | ((a&32)>>3) | ((a&64)>>5) | ((a&128)>>7)    //Reverse the order of the bits
@@ -47,3 +50,5 @@ inline void addReverseToQByteArray(QByteArray &byteArray, quint64 num) {
     byteArray.append(INVERT_BYTE(char(num>>48)));
     byteArray.append(INVERT_BYTE(char(num>>56)));
 }
+
+#endif // BYTEPLUS_H
